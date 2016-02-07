3e0788be5b0d584714076dd44722221b54abe96a
natedrake <john1o1grady@hotmail.com>
2016-02-07 14:45:50 +0000

    added datehelping functionality

--- OnTrack/src/DateHelper/DateHelper.cs
+++ OnTrack/src/DateHelper/DateHelper.cs
@@ -8,5 +8,39 @@ namespace OnTrack.src.DateHelper
 {
     class DateHelper
     {
+        private int milliSeconds;
+
+        // mutator / setter methods...
+        public void setMilliseconds(int milliSeconds)
+        {
+            this.milliSeconds = milliSeconds;
+        }
+
+
+        // helper / getter methods...
+        public int getMilliSeconds()
+        {
+            return this.milliSeconds;
+        }
+
+        public int getSeconds()
+        {
+            return this.milliSeconds / 1000;
+        }
+
+        public int getMinutes()
+        {
+            return this.getSeconds() / 60;
+        }
+
+        public int getHours()
+        {
+            return this.getMinutes() / 60;
+        }
+
+        public int getDays()
+        {
+            return this.getHours() / 24;
+        }
     }
 }
